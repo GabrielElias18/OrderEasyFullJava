57482ed7157e09e9ede245b42d75be3c990ddfce
Gabriel <113306327+GabrielElias18@users.noreply.github.com>
2024-11-01 20:10:57 -0500

    Creación de productos funcionando (falta solucionar alguito)

--- src/main/java/com/ejemplo/miproyecto/controllers/ProductController.java
+++ src/main/java/com/ejemplo/miproyecto/controllers/ProductController.java
@@ -3,6 +3,9 @@ package com.ejemplo.miproyecto.controllers;
 import com.ejemplo.miproyecto.models.Product;
 import com.ejemplo.miproyecto.models.Counter;
 import com.ejemplo.miproyecto.repositories.ProductRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import com.ejemplo.miproyecto.repositories.CounterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,21 +23,26 @@ public class ProductController {
     private CounterRepository counterRepository;
 
     @PostMapping("/productos")
-    public ResponseEntity<?> crearProducto(@RequestBody Product product) {
-        Counter counter = counterRepository.findByName("productoId");
-        if (counter == null) {
-            counter = new Counter();
-            counter.setName("productoId");
-            counter.setValue(0);
-        }
-        counter.setValue(counter.getValue() + 1);
-        counterRepository.save(counter);
-
-        product.setProductoId(counter.getValue());
-        productRepository.save(product);
-
-        return ResponseEntity.status(201).body("Producto creado exitosamente");
+    public ResponseEntity<?> crearProducto(@RequestBody Product product, HttpServletRequest request) {
+    int usuarioId = (int) request.getAttribute("usuarioId");
+    
+    Counter counter = counterRepository.findByName("productoId");
+    if (counter == null) {
+        counter = new Counter();
+        counter.setName("productoId");
+        counter.setValue(0);
     }
+    counter.setValue(counter.getValue() + 1);
+    counterRepository.save(counter);
+
+    product.setProductoId(counter.getValue());
+    product.setUsuarioId(usuarioId); // Establecer el usuarioId
+    productRepository.save(product);
+
+    return ResponseEntity.status(201).body("Producto creado exitosamente");
+}
+
+
 
     @GetMapping("/productos")
     public ResponseEntity<List<Product>> obtenerProductos(@RequestParam int usuarioId) {
